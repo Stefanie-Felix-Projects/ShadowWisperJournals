d55fd97ee9ea40b6b7ea00a87afd2038294b0abb
StefanieFelix <stefanie.seeck@gmx.de>
2025-01-02 09:09:56 +0100

    Initial Commit

---
+++ ShadowWisperJournals/ShadowWisperJournalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowWisperJournalsApp.swift
+//  ShadowWisperJournals
+//
+//  Created by Stefanie Seeck on 02.01.25.
+//
+
+import SwiftUI
+
+@main
+struct ShadowWisperJournalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShadowWisperJournalsTests/ShadowWisperJournalsTests.swift
@@ -0,0 +1,16 @@
+//
+//  ShadowWisperJournalsTests.swift
+//  ShadowWisperJournalsTests
+//
+//  Created by Stefanie Seeck on 02.01.25.
+//
+
+import Testing
+
+struct ShadowWisperJournalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
